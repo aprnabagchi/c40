26d3b29ddff30d0799336a95c78820a7a8b163e1
aprnabagchi <71764327+aprnabagchi@users.noreply.github.com>
2021-05-04 04:19:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # c40
+
+1# added all the images inside preload()function in sketch.js by using loadImage() function
+
+2# added images for the cars using addImage()function, track image using image()function and ground image using background()function
+
+3# added end condition inside game.js, created end()function and inside sketch.js we have defined whenever gameState reaches 2 it should call end()function
+
+4# added RESET button inside form.js, set the position and given the logic that whenever we press RESET button it should reset playerCount and gameState to 0 by using updateCount() and update() function from Player class and Game class respectively.
+
+
+That's it ....:)
---
+++ js/Game.js
@@ -0,0 +1,105 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+//inside game.js we are adding images in all the 4 car sprites
+    car1 = createSprite(100,200);
+    car1.addImage("car1",car1_img);
+    car2 = createSprite(300,200);
+    car2.addImage("car2",car2_img);
+    car3 = createSprite(500,200);
+    car3.addImage("car3",car3_img);
+    car4 = createSprite(700,200);
+    car4.addImage("car4",car4_img);
+    cars = [car1, car2, car3, car4];
+  }
+
+  play(){
+    form.hide();
+
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      //here we have added ground image
+      background(ground)
+      //var display_position = 100;
+      //here we have added image for the track by using image() function
+     // image(imageVariable name,xPos, yPos, width, height )
+      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+
+      //index of the array
+      var index =0;
+
+      //x and y position of the cars
+      var x =200;
+      var y;
+
+      for(var plr in allPlayers){
+        //add 1 to the index for every loop
+        index = index + 1 ;
+
+        //position the cars a little away from each other in x direction
+        x = x + 200;
+        //use data form the database to display the cars in y direction
+        y = displayHeight - allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+
+        if (index === player.index){
+          cars[index - 1].shapeColor = "red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y
+        }
+       
+        //textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+
+    //here we have defined condition that whenever player's distance will be
+    //greater than 3860 , it should reach to end state
+    if(player.distance > 3860){
+      //gameState 1 means serve state, 1 means play state and 2 means end state
+      gameState = 2;
+    }
+   
+    drawSprites();
+  }
+//we have defined end() function, this function will be called when gamestate 
+//changes to 2
+  end(){
+    console.log("Game Ended");
+  }
+}
